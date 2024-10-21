42749519558ba1650dcc901dfa163c70c545c746
Why You Visit My Profile 😂 <153857610+prf100rabhAaBb40769374divyanshi40769374@users.noreply.github.com>
2024-10-21 17:57:20 +0530

    [ 𝐏𝐑𝚯𝐅𝐄𝐒𝐒𝚯𝐑 ][ ѕαиαтαиι ]

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/xxxnxmxnsjjsjsbvwh252616562662640769374/PROFESSOR-MUSIC",
+    "https://github.com/prf100rabhAaBb40769374divyanshi40769374/PROFESSOR-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
