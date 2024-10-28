58899d06acc10cc0007d711ce97773fb334f691e
SYNAXAFSGAGHSHtsgshsjshxshssjaj40769374 <166273111+SYNAXAFSGAGHSHtsgshsjshxshssjaj40769374@users.noreply.github.com>
2024-10-28 19:50:02 +0530

    [ 𝐏𝐑𝚯𝐅𝐄𝐒𝐒𝚯𝐑 ][ ѕαиαтαиι ]

--- config.py
+++ config.py
@@ -44,15 +44,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/prf100rabhAaBb40769374divyanshi40769374/PROFESSOR-MUSIC",
+    "https://github.com/SYNAXAFSGAGHSHtsgshsjshxshssjaj40769374/GAMExMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Team_Heroku")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PROFESSORxNETWORK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+X8M_WnnUhz0xOTJl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+8JII918iFQJjNjM9")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
