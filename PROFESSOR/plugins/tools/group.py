1c0b1db7e771280afb5a2696713bb13cc0576770
JHYHDYHDUDJDJNEUBDUBTHEIOMWYI890 <theriyamusic94@gmail.com>
2024-11-27 15:12:14 +0530

    Update group.py

--- PROFESSOR/plugins/tools/group.py
+++ PROFESSOR/plugins/tools/group.py
@@ -5,30 +5,46 @@ from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-       await msg.reply("๏ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ.")
+    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+
+
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-       await msg.reply("**๏ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ.**")
+    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"๏ {message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-               text += f" [{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} ᰔᩚ")
-           except:
-             pass
+async def brah3(app: app, message: Message):
+    text = f"➻ {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ : :**\n\n**➻ **"
+    x = 0
+    for user in message.video_chat_members_invited.users:
+        try:
+            text += f"[{user.first_name}](tg://user?id={user.id}) "
+            x += 1
+        except Exception:
+            pass
+
+    try:
+        invite_link = await app.export_chat_invite_link(message.chat.id)
+        add_link = f"https://t.me/riyaXmusicbot?startgroup=true"
+        reply_text = f"{text} 🤭🤭"
+
+        await message.reply(
+            reply_text,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(text="✰Jᴏɪɴ ᴠᴄ ✰", url=add_link)],
+                ]
+            ),
+        )
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 ####
+
 
 @app.on_message(filters.command("math", prefixes="/"))
 def calculate_math(client, message):   
