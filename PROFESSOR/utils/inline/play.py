2fa4313d903d3d290b965a70463458b4d19c35e1
JHYHDYHDUDJDJNEUBDUBTHEIOMWYI890 <theriyamusic94@gmail.com>
2024-11-28 21:10:25 +0530

    Update play.py

--- PROFESSOR/utils/inline/play.py
+++ PROFESSOR/utils/inline/play.py
@@ -54,6 +54,13 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "▰▰▰▰▰▰▰▰▰▰"
     buttons = [
+                        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
                 [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
@@ -74,6 +81,13 @@ def stream_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, chat_id):
     buttons = [
+                        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
         [
             InlineKeyboardButton(text="✭ ᴜᴘᴅᴀᴛᴇ ❥", url="https://t.me/KSD_BOT_NETWORK",),
             InlineKeyboardButton(text="✭ sᴜᴘᴘᴏʀᴛ ❥", url="https://t.me/Team_riya_support",),
